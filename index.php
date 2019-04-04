fd3839a4723ce385c9627ebfd27a68cb51553ffe
mirarifhasan <arif.ishan05@gmail.com>
2019-04-04 19:28:21 +0600

    extention changed from html to php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <!-- Link -->
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.12/css/all.css" integrity="sha384-G0fIWCsCzJIMAVNQPfjH08cyYaUtMwjJwqiRKxxE/rx96Uroj1BtIQ6MLJuheaO9" crossorigin="anonymous">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/owl.carousel.min.css">
     <link rel="stylesheet" href="css/animate.min.css"> </head>
 
@@ -25,11 +25,11 @@
                         <div class="menu-area">
                             <div class="menu">
                                 <ul class="nav">
-                                    <li><a href="index.html"><b>Home</b></a></li>
-                                    <li><a href="about%20us.html"><b>About Us</b></a></li>
+                                    <li><a href="index.php"><b>Home</b></a></li>
+                                    <li><a href="about_us.php"><b>About Us</b></a></li>
                                     <li><a href="#"><b>My Profile</b></a></li>
-                                    <li><a href="contact.html"><b>Contact</b></a></li>
-                                    <li><a href="log%20in.html"><b>Login</b></a></li>
+                                    <li><a href="contact.php"><b>Contact</b></a></li>
+                                    <li><a href="log_in.php"><b>Login</b></a></li>
                                 </ul>
                             </div>
                         </div>
