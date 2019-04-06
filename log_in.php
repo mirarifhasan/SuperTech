f126c7e4cbfa2893320faf520d8ad9a4c45ca6e1
mirarifhasan <arif.ishan05@gmail.com>
2019-04-06 09:22:10 +0600

    dynamic slider, login partial

--- log_in.php
+++ log_in.php
@@ -1,7 +1,7 @@
 <?php
 
 session_start();
-session_destroy();
+//session_destroy();
 
 $host = 'localhost';
 $user = 'root';
@@ -11,7 +11,11 @@ $link = mysqli_connect($host, $user, $password, $db);
 
 $email='';
 $password='';
-$error='';
+$errorEmail=''; $errorPassword = ''; $errorUser='';
+
+function destroy(){
+    session_destroy();
+}
 
 function clean_text($string)
 {
@@ -20,33 +24,44 @@ function clean_text($string)
      $string = htmlspecialchars($string);
      return $string;
 }
+if(isset($_POST["logout"])){
+    destroy();
+}
 
 if(isset($_POST["login"])){
     
     if(empty($_POST["email"])){
-        $error .= '<p><label class="text-danger">Please Enter your Email</label></p>';
+        $errorEmail .= '<h2><label class="text-danger">Email is required</label></h2>';
     }
     else{
         $email = clean_text($_POST["email"]);
         if(!filter_var($email, FILTER_VALIDATE_EMAIL))
         {
-            $error .= '<p><label class="text-danger">Invalid email format</label></p>';
+            $errorEmail .= '<h2><label class="text-danger">Invalid email format</label></h2>';
         }
     }
     
     if(empty($_POST["password"])){
-        $error .= '<p><label class="text-danger">Password is required</label></p>';
+        $errorPassword .= '<h2><label class="text-danger">Password is required</label></h2>';
     }
     else{
         $password = clean_text($_POST["password"]);
     }
 
-    if($error == '') {
-        $email = '';
-        $password = '';
-        header('Location: index.php');
+    if($errorEmail == '' && $errorPassword=='') {
+        $sql = 'select * from user where email="'.$email.'" and password="'.$password.'"';
+        $result = mysqli_query($link, $sql);
+        $noOfData = mysqli_num_rows($result);
+        
+        if($noOfData>0){
+            $_SESSION['email'] = $email;
+            $_SESSION['password'] = $password;        
+            header('Location: index.php');
+        }else
+            $errorUser = '<h2><label class="text-danger">Wrong input</label></h2>';
     }
 }
+mysqli_close($link);
 ?>
 
 
@@ -71,7 +86,7 @@ if(isset($_POST["login"])){
             <div class="container">
                 <div class="row">
                     <div class="col-md-4">
-                        <div class="logo"> <img src="image/logo.png" class="img-fluid" alt=""> </div>
+                        <div class="logo"> <img src="image/icon/logo.png" class="img-fluid" alt=""> </div>
                     </div>
                     <div class="col-md-8">
                         <div class="menu-area">
@@ -83,11 +98,16 @@ if(isset($_POST["login"])){
                                     <li><a href="#"><b>My Profile</b></a></li>
                                     <li><a href="contact.php"><b>Contact</b></a></li>
                                     
-                                    <li>
-                                        <a href="log_in.php">
-                                            <button><b>Login</b></button>
-                                        </a>
-                                    </li>
+                                    <?php
+                                    if(isset($_SESSION['email']) != ''){
+                                        echo '<li><a href="index.php"><button name="logout"><b>Logout</b></button></a></li>';
+                                    }
+                                        
+                                    else 
+                                        echo '<li><a href="log_in.php"><button><b>Login</b></button></a></li>';
+                                    ?>
+                                    
+
                                 </ul>
                             </div>
                         </div>
@@ -98,19 +118,24 @@ if(isset($_POST["login"])){
     </div>
     <!-- login-area -->
     <div class="login-area">
-        <div class="loginbox"> <img src="image/001.jpg" alt="pic">
+        <div class="loginbox"> <img src="image/icon/001.jpg" alt="pic">
             <h1>Login Here</h1>
             <form method="post">
                 <p>Email</p>
                 <input type="text" name="email" placeholder="Enter Email" value="<?php echo $email; ?>">
+                <?php echo $errorEmail; ?>
+                
                 <p>Password</p>
-
                 <input type="password" name="password" placeholder="Enter Password" value="<?php echo $password; ?>">
-                <input type="submit" name="login" value="Login"> <a href="#">Lost your password?</a> <a href="sign_up.php">Don't have an account?</a> </form>
-
+                <?php echo $errorPassword; ?>
+                
+                <input type="submit" name="login" value="Login">
+                <?php echo $errorUser; ?>
+                <a href="sign_up.php">Don't have an account?</a> </form>
+                
         </div>
     </div>
-    <div class="arrow-top"> <img src="image/arrow.png" alt=""> </div>
+    <div class="arrow-top"> <img src="image/icon/arrow.png" alt=""> </div>
     
     <!-- Link -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
--- product.php
+++ product.php
@@ -19,7 +19,7 @@
             <div class="container">
                 <div class="row">
                     <div class="col-md-4">
-                        <div class="logo"> <img src="image/logo.png" class="img-fluid" alt=""> </div>
+                        <div class="logo"> <img src="image/icon/logo.png" class="img-fluid" alt=""> </div>
                     </div>
                     <div class="col-md-8">
                         <div class="menu-area">
@@ -83,7 +83,7 @@
     
     
 
-    <div class="arrow-top"> <img src="image/arrow.png" alt=""> </div>
+    <div class="arrow-top"> <img src="image/icon/arrow.png" alt=""> </div>
     <!-- Link -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
