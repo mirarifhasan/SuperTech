084c8c4c50d6c9b5a93d78f4844d9e00e504ba9d
kabirhasankafi <kabirhasankafi07@gmail.com>
2019-04-10 15:05:04 +0600

    cart page create

--- cart.php
+++ cart.php
@@ -14,17 +14,36 @@ $link = mysqli_connect($host, $user, $password, $db);
 $sl = 1;
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>SUPER TECH</title>
+    <!-- Link -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.12/css/all.css" integrity="sha384-G0fIWCsCzJIMAVNQPfjH08cyYaUtMwjJwqiRKxxE/rx96Uroj1BtIQ6MLJuheaO9" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/animate.min.css"> </head>
 
+<body>
 
-    <table cellpadding="10">
+<div class="cart-area">
+    <div class="container">
+        <div class="section-title">
+            <h2>Cart</h2>
+        </div>
+        <div class="table-box">
+               <table cellpadding="10">
         <tr>
             <th>Sr. No.</th>
             <th>Product Name</th>
             <!--<th>Product Quantity</th>-->
             <th>Product Price</th>
         </tr>
-
+        
         <?php
             foreach($_SESSION['Cart'] as $key) {
 
@@ -42,5 +61,24 @@ $sl = 1;
         ?>
 
     </table>
+       
+        </div>
+        <button><a href="checkout.php">Check Out</a></button>
+    </div>
+</div>
+ <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+<script src="js/jquery-3.3.1.min.js"></script>
+<script src="js/isotope.pkgd.min.js"></script>
+<script src="js/owl.carousel.min.js"></script>
+<script src="js/wow.min.js"></script>
+<script src="js/main.js"></script>
+<script>
+    new WOW().init();
+</script>
+</body>
 
-    <button><a href="checkout.php">Check Out</a></button>
+</html>
+
+    
